7b09beeaff114426f7dbe4b4629aba114311273c
Ubuntu <ubuntu@ip-172-31-49-29.ec2.internal>
2015-03-13 00:42:38 +0000

    merging

--- ass2_bash.sh
+++ ass2_bash.sh
@@ -11,6 +11,6 @@ for i in 1 2 4 8 16
         for n in `seq 1 20`;
             do
                 echo -e "\nthreads: $i"
-                java CoinFlip "$i" 100000000
+                java CoinFlip "$i" 1000000000
             done
     done 
